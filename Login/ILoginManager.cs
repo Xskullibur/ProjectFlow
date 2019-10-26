60cf5dc01908513a921eb9210847448144deab82
Peh Zi Heng <pehziheng@gmail.com>
2019-10-26 12:54:33 +0800

    Reorganize Login codes

--- Login/ILoginManager.cs
+++ Login/ILoginManager.cs
@@ -9,35 +9,6 @@ namespace ProjectFlow.Login
 {
 
     /// <summary>
-    /// LoginService used for login purpose.
-    /// LoginService depends on, <see cref="ILoginManager"/> and <see cref="IAuthCallback"/>
-    /// Pass both this classes as dependency into LoginService constructor
-    /// </summary>
-    public class LoginService
-    {
-        public LoginService(ILoginManager loginManager, IAuthCallback authCallback)
-        {
-            this.loginManager = loginManager;
-            this.authCallback = authCallback;
-        }
-
-        private ILoginManager loginManager { get; }
-        private IAuthCallback authCallback { get; }
-
-        /// <summary>
-        /// Authenticate user from the credential passed into this method
-        /// </summary>
-        /// <param name="page">Page where this this method called</param>
-        /// <param name="credential">Credential to authenticate the user</param>
-        public void Authenticate(Page page, LoginCredential credential)
-        {
-            var callbackEvent = new CallbackEvent(loginManager.HandleAuth(credential));
-            authCallback.AuthCallback(page, callbackEvent);
-        }
-
-    }
-
-    /// <summary>
     /// Perform and handle login authentication.
     /// Extends this class when you want a custom login authentication.
     /// Some ILoginManagers already defined:
@@ -67,72 +38,6 @@ namespace ProjectFlow.Login
         }
         
 
-    }
-
-    /// <summary>
-    /// Handle post authentication such as Redirecting user, creating new user etc
-    /// Some IAuthCallback already defined:
-    /// <see cref="AuthCallbacks.RedirectAuthCallback"/>
-    /// </summary>
-    public interface IAuthCallback
-    {
-        void AuthCallback(Page page, CallbackEvent callbackEvent);
-
-    }
-
-    public class CallbackEvent
-    {
-        public bool IsAuthenticated { get; }
-
-        internal protected CallbackEvent(bool isAuthenticated)
-        {
-            IsAuthenticated = isAuthenticated;
-        }
-
-    }
-
-    namespace LoginManagers
-    {
-        public class BasicLoginManager : ILoginManager
-        {
-            public bool HandleAuth(LoginCredential credential)
-            {
-                //TODO handle login from user and check if user exists
-                return credential.Username == "Hello" && credential.PasswordHashed == "Password";
-            }
-        }
-
-        public class GoogleLoginManager : ILoginManager
-        {
-            public static string RedirectionUrl = "";
-            public static string ClientId = "";
-            public static string ClientSecret = "";
-
-            public bool HandleAuth(LoginCredential credential)
-            {
-                throw new NotImplementedException();
-            }
-        }
-    }
-
-    namespace AuthCallbacks
-    {
-        public class RedirectAuthCallback : IAuthCallback
-        {
-
-            private string Path;
-
-            public RedirectAuthCallback(string path)
-            {
-                Path = path;
-            }
-
-            public void AuthCallback(Page page, CallbackEvent callbackEvent)
-            {
-                if(callbackEvent.IsAuthenticated)page.Response.Redirect(Path);
-                else page.Response.Redirect("Failed");
-            }
-        }
     }
 
 
