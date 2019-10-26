efe73658a35499a9fd1969d75ce073c2568283ce
Peh Zi Heng <pehziheng@gmail.com>
2019-10-26 23:26:15 +0800

    Change folder structure

---
+++ ProjectFlow/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProjectFlow.Global" Language="C#" %>
---
+++ ProjectFlow/Login/RedirectAuthCallback.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+
+namespace ProjectFlow.Login
+{
+    public class RedirectAuthCallback : IAuthCallback
+    {
+
+        private string Path;
+
+        public RedirectAuthCallback(string path)
+        {
+            Path = path;
+        }
+
+        public void AuthCallback(Page page, CallbackEvent callbackEvent)
+        {
+            if (callbackEvent.IsAuthenticated) page.Response.Redirect(Path);
+            else page.Response.Redirect("Failed");
+        }
+    }
+}
