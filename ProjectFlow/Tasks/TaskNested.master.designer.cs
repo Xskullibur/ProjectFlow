e1540c1f71667f1110708b6cfcd3d509812f8a41
Xskullibur <simasas2222@gmail.com>
2019-11-05 22:39:54 +0800

    Added  Nested Master Page

--- ProjectFlow/Tasks/TaskNested.master.designer.cs
+++ ProjectFlow/Tasks/TaskNested.master.designer.cs
@@ -7,10 +7,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ProjectFlow {
+namespace ProjectFlow.Tasks {
     
     
-    public partial class ServicesWithContent {
+    public partial class TaskNested {
         
         /// <summary>
         /// head control.
@@ -20,24 +20,6 @@ namespace ProjectFlow {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
-        
-        /// <summary>
-        /// form1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-        
-        /// <summary>
-        /// ListView1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ListView ListView1;
         
         /// <summary>
         /// MainContent control.
