3665bbe019008a7924f2d4643db2b6f889c22832
Xskullibur <simasas2222@gmail.com>
2019-11-09 19:41:49 +0800

    Update nested master [Not Working]

--- ProjectFlow/Global.asax.cs
+++ ProjectFlow/Global.asax.cs
@@ -16,7 +16,6 @@ namespace ProjectFlow
 
         protected void Application_Start(object sender, EventArgs e)
         {
-
             ScriptManager.ScriptResourceMapping.AddDefinition("jquery", new ScriptResourceDefinition
             {
                 Path = "~/Scripts/jquery-3.4.1.min.js",
