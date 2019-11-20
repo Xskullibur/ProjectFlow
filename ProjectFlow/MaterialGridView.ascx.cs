15db86b1ecb34e9752e624832b106a34a446a4bd
xskullibur <simasas2222@gmail.com>
2019-11-20 10:33:21 +0800

    WIP Material Design GridView

---
+++ ProjectFlow/MaterialGridView.ascx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProjectFlow
+{
+    public partial class MaterialGridView : System.Web.UI.UserControl
+    {
+        public const string NURMEIC = "mdc-data-table_header-cell--numeric";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            AddColumn("Hi ALson", "");
+            AddColumn("Hi ALson", "");
+            AddColumn("Hi ALson", "");
+        }
+
+        private void AddColumn(string headerName, string type)
+        {
+            string containerId = Container.ClientID;
+            Response.Write(@"
+                <script language='javascript'>
+                    window.addEventListener('load', function(){
+                        var header = $('#" + containerId + @" .mdc-data-table__header-row')
+                        header.append('<th class=\'mdc-data-table__header-cell\' role=\'columnheader\' scope=\'col\'>" + headerName + @"</th>')
+                    });
+                </script>");
+        }
+
+    }
+}
--- ProjectFlow/Tasks/tDetailedView.aspx
+++ ProjectFlow/Tasks/tDetailedView.aspx
@@ -1,5 +1,8 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Tasks/TaskNested.master" AutoEventWireup="true" CodeBehind="tDetailedView.aspx.cs" Inherits="ProjectFlow.Tasks.tDetailedView" %>
 
+<%-- Register Material Grid View --%>
+<%@ Register Src="~/MaterialGridView.ascx" TagName="MaterialGridView" TagPrefix="ProjectFlow" %>
+
 <%--TaskNested Master--%>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
@@ -12,6 +15,10 @@
         <div class="row">
             <div class="col">
                 Detailed View
+
+                <ProjectFlow:MaterialGridView runat="server">
+
+                </ProjectFlow:MaterialGridView>
 
                 <div class="mdc-data-table">  
                     <asp:GridView ID="taskView" runat="server" >
