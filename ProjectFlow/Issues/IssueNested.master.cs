f4605d50a21dc6bc78d0dc9884b0eb04e54867b8
bryan <182869n@mymail.nyp.edu.sg>
2019-12-17 17:40:05 +0800

    created master page and web fom for user issues

---
+++ ProjectFlow/Issues/IssueNested.master.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProjectFlow.Issues
+{
+    public partial class IssueNested : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            taskView.DataSource = new List<string>()
+            {
+                "TESTMEM1",
+                "TESTMEM2",
+                "TESTMEM3",
+                "TESTMEM4",
+                "TESTMEM5"
+            };
+            taskView.DataBind();
+        }
+    }
+}
---
+++ ProjectFlow/Issues/tConflictRes2.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Issues/IssueNested.master" AutoEventWireup="true" CodeBehind="tConflictRes2.aspx.cs" Inherits="ProjectFlow.Issues.tConflictRes2" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:Button ID="btnRandom" CssClass ="btn btn-secondary" runat="server" Text="Random" />
+</asp:Content>
