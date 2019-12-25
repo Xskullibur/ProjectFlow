9d7435a1efa8685e7f90057130c7d95eaa381cf5
Xskullibur <simasas2222@gmail.com>
2019-12-25 14:57:18 +0800

    +Added Success to Alert +Added Alert to ServicesWithContent

--- ProjectFlow/ServicesWithContent.Master.designer.cs
+++ ProjectFlow/ServicesWithContent.Master.designer.cs
@@ -31,6 +31,24 @@ namespace ProjectFlow {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// AlertsUpdatePanel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.UpdatePanel AlertsUpdatePanel;
+        
+        /// <summary>
+        /// AlertsPlaceHolder control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel AlertsPlaceHolder;
+        
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
