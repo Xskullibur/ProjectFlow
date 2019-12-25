c547ff341762c1aabb10feb4c01ec31dd3252baa
lee hsien xiang <leehsienx@gmail.com>
2019-12-25 16:58:02 +0800

    separated navbar from servicewithcontent

--- ProjectFlow/NavBar.Master.designer.cs
+++ ProjectFlow/NavBar.Master.designer.cs
@@ -10,7 +10,7 @@
 namespace ProjectFlow {
     
     
-    public partial class ServicesWithContent {
+    public partial class NavBar {
         
         /// <summary>
         /// head control.
@@ -20,6 +20,15 @@ namespace ProjectFlow {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
+        
+        /// <summary>
+        /// form1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
         /// ContentPlaceHolder1 control.
