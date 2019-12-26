69a258693e3067f2fb463d8b68ca337246bd354b
Xskullibur <simasas2222@gmail.com>
2019-12-26 14:41:32 +0800

    +EmailHelper Added

---
+++ ProjectFlow/Utils/EmailHelper.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.Net.Mail;
+using System.Configuration;
+
+namespace ProjectFlow.Utils
+{
+    public class EmailHelper
+    {
+        string sender = ConfigurationManager.AppSettings["Email"].ToString();
+        string senderPW= ConfigurationManager.AppSettings["EmailPW"].ToString();
+        string smtp= ConfigurationManager.AppSettings["SMTP"].ToString();
+        int portNo = Convert.ToInt32(ConfigurationManager.AppSettings["PortNo"].ToString());
+
+        /// <summary>
+        /// Send Email
+        /// </summary>
+        /// <param name="recivers">List of Reciver's Emails</param>
+        /// <param name="subject">Subject of Email</param>
+        /// <param name="mailText">Body of the Email</param>
+        /// <returns>Boolean</returns>
+        public bool SendEmail(List<string> recivers, string subject, string mailText)
+        {
+            try
+            {
+                MailMessage _mailMessage = new MailMessage();
+                _mailMessage.IsBodyHtml = true;
+                _mailMessage.From = new MailAddress(sender);
+
+                foreach (string reciver in recivers)
+                {
+                    _mailMessage.To.Add(reciver);
+                }
+
+                _mailMessage.Subject = subject;
+                _mailMessage.Body = mailText;
+
+                SmtpClient _smtp = new SmtpClient();
+                _smtp.Host = smtp;
+                _smtp.Port = portNo;
+                _smtp.EnableSsl = true;
+
+                NetworkCredential _network = new NetworkCredential(sender, senderPW);
+                _smtp.Credentials = _network;
+
+                _smtp.Send(_mailMessage);
+
+                return true;
+
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine(e.Message);
+                return false;
+            }
+        }
+
+    }
+}
