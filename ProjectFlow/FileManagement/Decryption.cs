72799b5998e46e6e75a4994d5c34ee05825a7bf0
lee hsien xiang <leehsienx@gmail.com>
2020-01-12 22:01:59 +0800

    added decryption logic

---
+++ ProjectFlow/FileManagement/Decryption.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace ProjectFlow.FileManagement
+{
+    public class Decryption
+    {
+        string IV = "WTVqdWZnTHpxTnl1";
+        public void DecryptFileWithKey(string path, string key)
+        {
+            byte[] fileToByte = File.ReadAllBytes(path);
+            using (var AES = new AesCryptoServiceProvider())
+            {
+                AES.IV = Encoding.UTF8.GetBytes(IV);
+                AES.Key = Encoding.UTF8.GetBytes(key);
+                AES.Mode = CipherMode.CBC;
+                AES.Padding = PaddingMode.PKCS7;
+
+                using (var memStream = new MemoryStream())
+                {
+                    CryptoStream cryptoStream = new CryptoStream(memStream, AES.CreateDecryptor(), CryptoStreamMode.Write);
+                    cryptoStream.Write(fileToByte, 0, fileToByte.Length);
+                    cryptoStream.FlushFinalBlock();
+                    File.WriteAllBytes(path, memStream.ToArray());
+                }
+            }
+        }
+    }
+}
