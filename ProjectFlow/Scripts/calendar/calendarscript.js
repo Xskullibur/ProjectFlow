0434c4f0ab06d924b3cfa0cd56a7204874e44db8
Xskullibur <simasas2222@gmail.com>
2020-01-26 16:10:20 +0800

    CalendarView Remove All Events

--- ProjectFlow/Scripts/calendar/calendarscript.js
+++ ProjectFlow/Scripts/calendar/calendarscript.js
@@ -3,118 +3,6 @@ var addStartDate;
 var addEndDate;
 var globalAllDay;
 
-function updateEvent(event, element) {
-    //alert(event.description);
-
-    if ($(this).data("qtip")) $(this).qtip("destroy");
-
-    currentUpdateEvent = event;
-
-    $('#updatedialog').dialog('open');
-    $("#eventName").val(event.title);
-    $("#eventDesc").val(event.description);
-    $("#eventId").val(event.id);
-    $("#eventStart").text("" + event.start.toLocaleString());
-
-    if (event.end === null) {
-        $("#eventEnd").text("");
-    }
-    else {
-        $("#eventEnd").text("" + event.end.toLocaleString());
-    }
-
-    return false;
-}
-
-function updateSuccess(updateResult) {
-    //alert(updateResult);
-}
-
-function deleteSuccess(deleteResult) {
-    //alert(deleteResult);
-}
-
-function addSuccess(addResult) {
-// if addresult is -1, means event was not added
-//    alert("added key: " + addResult);
-
-    if (addResult != -1) {
-        $('#calendar').fullCalendar('renderEvent',
-						{
-						    title: $("#addEventName").val(),
-						    start: addStartDate,
-						    end: addEndDate,
-						    id: addResult,
-						    description: $("#addEventDesc").val(),
-						    allDay: globalAllDay
-						},
-						true // make the event "stick"
-					);
-
-
-        $('#calendar').fullCalendar('unselect');
-    }
-}
-
-function UpdateTimeSuccess(updateResult) {
-    //alert(updateResult);
-}
-
-function selectDate(start, end, allDay) {
-
-    $('#addDialog').dialog('open');
-    $("#addEventStartDate").text("" + start.toLocaleString());
-    $("#addEventEndDate").text("" + end.toLocaleString());
-
-    addStartDate = start;
-    addEndDate = end;
-    globalAllDay = allDay;
-    //alert(allDay);
-}
-
-function updateEventOnDropResize(event, allDay) {
-
-    //alert("allday: " + allDay);
-    var eventToUpdate = {
-        id: event.id,
-        start: event.start
-    };
-
-    if (event.end === null) {
-        eventToUpdate.end = eventToUpdate.start;
-    }
-    else {
-        eventToUpdate.end = event.end;
-    }
-
-    var endDate;
-    if (!event.allDay) {
-        endDate = new Date(eventToUpdate.end + 60 * 60000);
-        endDate = endDate.toJSON();
-    }
-    else {
-        endDate = eventToUpdate.end.toJSON();
-    }
-
-    eventToUpdate.start = eventToUpdate.start.toJSON();
-    eventToUpdate.end = eventToUpdate.end.toJSON(); //endDate;
-    eventToUpdate.allDay = event.allDay;
-
-    PageMethods.UpdateEventTime(eventToUpdate, UpdateTimeSuccess);
-}
-
-function eventDropped(event, dayDelta, minuteDelta, allDay, revertFunc) {
-    if ($(this).data("qtip")) $(this).qtip("destroy");
-
-    updateEventOnDropResize(event);
-}
-
-function eventResized(event, dayDelta, minuteDelta, revertFunc) {
-    if ($(this).data("qtip")) $(this).qtip("destroy");
-
-    updateEventOnDropResize(event);
-}
-
 function checkForSpecialChars(stringToCheck) {
     var pattern = /[^A-Za-z0-9 ]/;
     return pattern.test(stringToCheck); 
@@ -147,72 +35,6 @@ function qTipText(start, end, description) {
 }
 
 $(document).ready(function() {
-    // update Dialog
-    $('#updatedialog').dialog({
-        autoOpen: false,
-        width: 470,
-        buttons: {
-            "update": function() {
-                //alert(currentUpdateEvent.title);
-                var eventToUpdate = {
-                    id: currentUpdateEvent.id,
-                    title: $("#eventName").val(),
-                    description: $("#eventDesc").val()
-                };
-
-                if (checkForSpecialChars(eventToUpdate.title) || checkForSpecialChars(eventToUpdate.description)) {
-                    alert("please enter characters: A to Z, a to z, 0 to 9, spaces");
-                }
-                else {
-                    PageMethods.UpdateEvent(eventToUpdate, updateSuccess);
-                    $(this).dialog("close");
-
-                    currentUpdateEvent.title = $("#eventName").val();
-                    currentUpdateEvent.description = $("#eventDesc").val();
-                    $('#calendar').fullCalendar('updateEvent', currentUpdateEvent);
-                }
-
-            },
-            "delete": function() {
-
-                if (confirm("do you really want to delete this event?")) {
-
-                    PageMethods.deleteEvent($("#eventId").val(), deleteSuccess);
-                    $(this).dialog("close");
-                    $('#calendar').fullCalendar('removeEvents', $("#eventId").val());
-                }
-            }
-        }
-    });
-
-    //add dialog
-    $('#addDialog').dialog({
-        autoOpen: false,
-        width: 470,
-        buttons: {
-            "Add": function() {
-                //alert("sent:" + addStartDate.format("dd-MM-yyyy hh:mm:ss tt") + "==" + addStartDate.toLocaleString());
-                var eventToAdd = {
-                    title: $("#addEventName").val(),
-                    description: $("#addEventDesc").val(),
-                    start: addStartDate.toJSON(),
-                    end: addEndDate.toJSON(),
-
-                    allDay: isAllDay(addStartDate, addEndDate)
-                };
-                
-                if (checkForSpecialChars(eventToAdd.title) || checkForSpecialChars(eventToAdd.description)) {
-                    alert("please enter characters: A to Z, a to z, 0 to 9, spaces");
-                }
-                else {
-                    //alert("sending " + eventToAdd.title);
-
-                    PageMethods.addEvent(eventToAdd, addSuccess);
-                    $(this).dialog("close");
-                }
-            }
-        }
-    });
 
     var date = new Date();
     var d = date.getDate();
@@ -227,17 +49,13 @@ $(document).ready(function() {
         header: {
             left: 'today',
             center: 'title',
-            right: 'prev next'
+            right: 'prev,next'
         },
         defaultView: 'month',
-        eventClick: updateEvent,
         selectable: true,
         selectHelper: true,
-        select: selectDate,
-        editable: true,
+        editable: false,
         events: "JsonResponse.ashx",
-        eventDrop: eventDropped,
-        eventResize: eventResized,
         eventRender: function(event, element) {
             element.qtip({
                 content: {
