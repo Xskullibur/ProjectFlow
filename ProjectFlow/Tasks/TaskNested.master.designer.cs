1139ba01b8176702681d5fa12f332887be89e085
Xskullibur <simasas2222@gmail.com>
2020-01-27 13:34:04 +0800

    Add Filter Panel

--- ProjectFlow/Tasks/TaskNested.master.designer.cs
+++ ProjectFlow/Tasks/TaskNested.master.designer.cs
@@ -31,15 +31,6 @@ namespace ProjectFlow.Tasks {
         protected global::System.Web.UI.WebControls.DropDownList taskViewDDL;
         
         /// <summary>
-        /// personalChkBx control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.CheckBox personalChkBx;
-        
-        /// <summary>
         /// addTaskBtn control.
         /// </summary>
         /// <remarks>
@@ -47,6 +38,15 @@ namespace ProjectFlow.Tasks {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button addTaskBtn;
+        
+        /// <summary>
+        /// personalChkBx control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox personalChkBx;
         
         /// <summary>
         /// modalUpdatePanel control.
